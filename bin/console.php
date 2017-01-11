0bc573db8152125a967a7a99419bacbe788fa874
igor <igor.sv.n@gmail.com>
2017-01-11 08:53:47 +0300

    Initial commit.

---
+++ bin/console.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2017, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+
+$app = new Application();
---
+++ src/Darvin/Databaser/Command/DownloadCommand.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2017, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\Databaser\Command;
+
+use Symfony\Component\Console\Command\Command;
+
+/**
+ * Download command
+ */
+class DownloadCommand extends Command
+{
+
+}
