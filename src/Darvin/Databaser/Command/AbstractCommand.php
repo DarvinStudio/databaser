4e6bda507ac3906051ee2fd063a01390733c5d6d
igor <igor.sv.n@gmail.com>
2017-01-12 12:07:34 +0300

    Extract abstract command class.

---
+++ src/Darvin/Databaser/Command/AbstractCommand.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2017, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\Databaser\Command;
+
+use Darvin\Databaser\Manager\LocalManager;
+use Darvin\Databaser\Manager\ManagerInterface;
+use Darvin\Databaser\Manager\RemoteManager;
+use Darvin\Databaser\SSH\SSHClient;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * Command abstract implementation
+ */
+abstract class AbstractCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setDefinition([
+            new InputArgument('ssh_user', InputArgument::REQUIRED),
+            new InputArgument('ssh_host', InputArgument::REQUIRED),
+            new InputArgument('project_path_remote', InputArgument::REQUIRED),
+            new InputArgument('project_path_local', InputArgument::REQUIRED),
+            new InputArgument('ssh_port', InputArgument::OPTIONAL, '', 22),
+            new InputOption('ssh_key', 'k', InputOption::VALUE_OPTIONAL, '', '.ssh/id_rsa'),
+        ]);
+    }
+
+    /**
+     * @param \Darvin\Databaser\Manager\ManagerInterface $manager Manager
+     *
+     * @return string
+     */
+    protected function createDumpFilename(ManagerInterface $manager)
+    {
+        return sprintf('%s_%s.sql.gz', $manager->getDbName(), (new \DateTime())->format('d-m-Y_H-i-s'));
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input Input
+     *
+     * @return \Darvin\Databaser\Manager\LocalManager
+     */
+    protected function createLocalManager(InputInterface $input)
+    {
+        return new LocalManager($input->getArgument('project_path_local'));
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input Input
+     *
+     * @return \Darvin\Databaser\Manager\RemoteManager
+     */
+    protected function createRemoteManager(InputInterface $input)
+    {
+        return new RemoteManager(
+            new SSHClient(
+                $input->getArgument('ssh_user'),
+                $input->getArgument('ssh_host'),
+                $input->getOption('ssh_key'),
+                $input->getArgument('ssh_port')
+            ),
+            $input->getArgument('project_path_remote')
+        );
+    }
+}
--- src/Darvin/Databaser/Command/PullCommand.php
+++ src/Darvin/Databaser/Command/PullCommand.php
@@ -10,37 +10,23 @@
 
 namespace Darvin\Databaser\Command;
 
-use Darvin\Databaser\Manager\LocalManager;
-use Darvin\Databaser\Manager\ManagerInterface;
-use Darvin\Databaser\Manager\RemoteManager;
-use Darvin\Databaser\SSH\SSHClient;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 /**
  * Pull command
  */
-class PullCommand extends Command
+class PullCommand extends AbstractCommand
 {
     /**
      * {@inheritdoc}
      */
     protected function configure()
     {
-        $this
-            ->setName('pull')
-            ->setDefinition([
-                new InputArgument('ssh_user', InputArgument::REQUIRED),
-                new InputArgument('ssh_host', InputArgument::REQUIRED),
-                new InputArgument('project_path_remote', InputArgument::REQUIRED),
-                new InputArgument('project_path_local', InputArgument::REQUIRED),
-                new InputArgument('ssh_port', InputArgument::OPTIONAL, '', 22),
-                new InputOption('ssh_key', 'k', InputOption::VALUE_OPTIONAL, '', '.ssh/id_rsa'),
-            ]);
+        parent::configure();
+
+        $this->setName('pull');
     }
 
     /**
@@ -50,20 +36,10 @@ class PullCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $projectPathRemote = $input->getArgument('project_path_remote');
-
-        $remoteManager = new RemoteManager(
-            new SSHClient(
-                $input->getArgument('ssh_user'),
-                $input->getArgument('ssh_host'),
-                $input->getOption('ssh_key'),
-                $input->getArgument('ssh_port')
-            ),
-            $projectPathRemote
-        );
+        $remoteManager = $this->createRemoteManager($input);
 
         $filename = $this->createDumpFilename($remoteManager);
-        $pathname = implode(DIRECTORY_SEPARATOR, [$projectPathRemote, $filename]);
+        $pathname = implode(DIRECTORY_SEPARATOR, [$input->getArgument('project_path_remote'), $filename]);
 
         $io->comment('Dumping remote database...');
 
@@ -73,7 +49,7 @@ class PullCommand extends Command
 
         $remoteManager->getFile($pathname, $filename);
 
-        $localManager = new LocalManager($input->getArgument('project_path_local'));
+        $localManager = $this->createLocalManager($input);
 
         if (!$localManager->databaseIsEmpty()) {
             $io->comment('Dumping local database...');
@@ -88,15 +64,5 @@ class PullCommand extends Command
         $io->comment('Importing remote database dump into the local database...');
 
         $localManager->importDump($filename);
-    }
-
-    /**
-     * @param \Darvin\Databaser\Manager\ManagerInterface $manager Manager
-     *
-     * @return string
-     */
-    private function createDumpFilename(ManagerInterface $manager)
-    {
-        return sprintf('%s_%s.sql.gz', $manager->getDbName(), (new \DateTime())->format('d-m-Y_H-i-s'));
     }
 }
